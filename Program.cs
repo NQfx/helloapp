61c2de5b025f875ba26a5d72ff2ef69dd6979f50
black <black@example.com>
2026-07-04 21:20:14 +0500

    Add calls to greeting methods

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ public static class Program
     public static void Main()
     {
         Console.WriteLine("hello everyone");
+        greetWhite();
+        greetBlack();
     }
 
     public static void greetWhite() => Console.WriteLine("hello white");
